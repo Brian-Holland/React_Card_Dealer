dd76ec2dfd16a7c7e46a652a636e1b14573733b1
Brian <brian.holland.pr@gmail.com>
2020-08-10 00:32:08 -0700

    initial commit

---
+++ src/Deck.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Deck extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Dealer</h1>
+			</div>
+		);
+	}
+}
+
+export default Deck;
