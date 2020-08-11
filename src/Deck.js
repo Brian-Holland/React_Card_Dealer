858c679433f364d162ab306cfdcabbc7e66e4776
Brian <brian.holland.pr@gmail.com>
2020-08-10 18:15:18 -0700

    added getcard to deck comp

--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-export class Deck extends Component {
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
+
+class Deck extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			deck: 'null',
+			drawnCards: []
+		};
+		this.getCard = this.getCard.bind(this);
+	}
+
+	async componentDidMount() {
+		//suffle deck
+		let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+		//set deck info in state
+		this.setState({
+			deck: deck.data
+		});
+	}
+
+	async getCard() {
+		//get deck id from api
+		let deck_id = this.state.deck.deck_id;
+
+		try {
+			//create url using base url to draw a card
+			let cardURL = `${API_BASE_URL}/${deck_id}/draw/`;
+			//wait to draw card from api
+			let cardRes = await axios.get(cardURL);
+			//throw error if deck is empty
+			if (!cardRes.data.success) {
+				throw new Error('Deck is empty');
+			}
+			//save card drawn to its own variable
+			let card = cardRes.data.cards[0];
+			//add card drawn to state with card info
+			this.setState((st) => ({
+				drawnCards: [
+					...st.drawnCards,
+					{
+						id: card.code,
+						image: card.image,
+						name: `${card.value} of ${card.suit}`
+					}
+				]
+			}));
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Dealer</h1>
+				<button onClick={this.getCard}>Deal Card</button>
 			</div>
 		);
 	}
